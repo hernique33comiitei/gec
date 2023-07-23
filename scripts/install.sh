b606897b0deadc5549659ece42452040bcaf6deb
hernique33comiitei <mulekinb@gmail.com>
2023-07-22 22:35:34 -0300

    🎉 Feature: Add script for install the gec

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# Set the full path to the installation directory (changed to /usr/local/bin/)
+INSTALL_DIR="/usr/local/bin/"
+
+# Clone the repository from GitHub
+git clone https://github.com/hernique33comiitei/gec.git "$INSTALL_DIR"
+
+# Create an alias for the gec script
+echo "alias gec='python ${INSTALL_DIR}/gec/index.py'" >> ~/.bashrc
+
+# Load changes from the shell configuration file
+source ~/.bashrc
+
+echo "The installation is complete! You can now use the 'gce' command to run the script."
