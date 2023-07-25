ba733e48b76f2a19c4a30159133b410f25d27aa2
hernique33comiitei <mulekinb@gmail.com>
2023-07-25 10:48:34 -0300

    ⚙  Configuration: Added code to download the necessary dependencies

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# Set the full path to the installation directory 
+# Set the full path to the installation directory
 INSTALL_DIR="$HOME/bin/gec"
 SHELL_USED=$(basename "$SHELL")
 
 # Clone the repository from GitHub
 git clone https://github.com/hernique33comiitei/gec.git "$INSTALL_DIR"
 
-# Create an alias for the gec script 
+# Change directory to the installation directory
+cd "$INSTALL_DIR"
 
+# Install the required libraries using pip
+python3 -m pip install -r requirements.txt
+
+# Create an alias for the gec script
 if [ "$SHELL_USED" = "bash" ]; then
   echo "alias gec='python3 ${INSTALL_DIR}/index.py'" >> ~/.bashrc
   source ~/.bashrc
@@ -19,8 +24,4 @@ if [ "$SHELL_USED" = "zsh" ]; then
   source ~/.zshrc
 fi
 
-echo "$SHELL_USED"
-
-
 echo "The installation is complete! You can now use the 'gec' command to run the script."
-
